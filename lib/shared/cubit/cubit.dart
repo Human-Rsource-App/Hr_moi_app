a269b2c27f7bb6dcbea5274655715f35737a361f
HA12602 <HA12602@gateway.com.tr>
2025-10-07 12:15:40 +0300

    create bloc constructor

--- lib/shared/cubit/cubit.dart
+++ lib/shared/cubit/cubit.dart
@@ -1,6 +1,8 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hr_moi/shared/cubit/states.dart';
 
 class HrMoiCubit extends Cubit<HrMoiStates> {
   HrMoiCubit() : super(InitialState());
+  static HrMoiCubit get(BuildContext context) => BlocProvider.of(context);
 }
