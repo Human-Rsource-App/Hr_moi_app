b91a96f40da50142c776d0a71103c411f1ababf6
HA12602 <HA12602@gateway.com.tr>
2025-10-13 13:26:53 +0300

    we will start from here to configure mrz

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hr_moi/modules/auth/registeration/nfc.dart';
+import 'package:hr_moi/modules/auth/registeration/otp_screen.dart';
 import 'package:hr_moi/shared/style/styles.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: lightTheme,
       home: Directionality(
         textDirection: TextDirection.rtl,
-        child: NfcReaderScreen(),
+        child: PinCodeVerificationScreen(),
       ),
     );
   }
